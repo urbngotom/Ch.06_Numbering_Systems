82b66bddb5468cd2ccfa6099f6ff293e476874fc
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-21 09:56:45 -0600

    Create 6.0_Take_Home_Test.py

---
+++ 6.0_Take_Home_Test.py
@@ -0,0 +1,15 @@
+'''
+Convert Binary 101010 to Hexadecimal.
+
+Convert Binary 11101001 to Octal.
+
+Convert Hexadecimal FC to Binary.
+
+Convert Hexadecimal 1F to Decimal.
+
+Convert Hexadecimal #FAAFBD to RGB.
+
+Convert Octal 70 to Binary.
+
+Convert RGB (32,128,64) to Hexadecimal.
+'''
