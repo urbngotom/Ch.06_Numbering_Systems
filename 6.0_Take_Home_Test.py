538746db7b154436114b5b30a867700abb04071a
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-21 10:02:32 -0600

    Update 6.0_Take_Home_Test.py

--- 6.0_Take_Home_Test.py
+++ 6.0_Take_Home_Test.py
@@ -1,4 +1,8 @@
 '''
+HONOR CODE: I solemnly promise that while taking this test I will only use any resources except my brain.
+Signed: ______________________
+
+
 Convert Binary 101010 to Hexadecimal.
 
 Convert Binary 11101001 to Octal.
@@ -12,4 +16,7 @@ Convert Hexadecimal #FAAFBD to RGB.
 Convert Octal 70 to Binary.
 
 Convert RGB (32,128,64) to Hexadecimal.
+
+Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
+
 '''
