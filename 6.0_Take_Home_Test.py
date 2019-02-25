b6a1dd2ac2dd4f06536a04d53d453624deb3f3bb
Marc Hermon <hermonm@urbandaleschools.com>
2019-02-25 12:17:29 -0600

    Update 6.0_Take_Home_Test.py

--- 6.0_Take_Home_Test.py
+++ 6.0_Take_Home_Test.py
@@ -3,20 +3,20 @@ HONOR CODE: I solemnly promise that while taking this test I will only use any r
 Signed: ______________________
 
 
-Convert Binary 101010 to Hexadecimal.
+1.) Convert Binary 101010 to Hexadecimal.
 
-Convert Binary 11101001 to Octal.
+2.) Convert Binary 11101001 to Octal.
 
-Convert Hexadecimal FC to Binary.
+3.) Convert Hexadecimal FC to Binary.
 
-Convert Hexadecimal 1F to Decimal.
+4.) Convert Hexadecimal 1F to Decimal.
 
-Convert Hexadecimal #FAAFBD to RGB.
+5.) Convert Hexadecimal #FAAFBD to RGB.
 
-Convert Octal 70 to Binary.
+6.) Convert Octal 70 to Binary.
 
-Convert RGB (32,128,64) to Hexadecimal.
+7.) Convert RGB (32,128,64) to Hexadecimal.
 
-Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
+8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
 
 '''
