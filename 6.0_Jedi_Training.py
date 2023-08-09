2a5d9a38e8672f156f1ca686a6fe2e3516a58f1f
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-09 17:13:21 -0500

    Update 6.0_Jedi_Training.py

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -1,21 +1,23 @@
 '''
-Sign your name:________________
+6.0 Jedi Training 15pts)  Name:________________
 
 
-1.) Convert Binary 101010 to Hexadecimal.
+1.) Convert Binary 101010 to Hexadecimal. (1pt)
 
-2.) Convert Binary 11101001 to Octal.
+2.) Convert Binary 11101001 to Octal. (1pt)
 
-3.) Convert Hexadecimal FC to Binary.
+3.) Convert Hexadecimal FC to Binary. (1pt)
 
-4.) Convert Hexadecimal 1F to Decimal.
+4.) Convert Hexadecimal 1F to Decimal. (1pt)
 
-5.) Convert Hexadecimal #FAAFBD to RGB.
+5.) Convert Hexadecimal #FAAFBD to RGB. (1pt)
 
-6.) Convert Octal 70 to Binary.
+6.) Convert Octal 70 to Binary. (1pt)
 
-7.) Convert RGB (32,128,64) to Hexadecimal.
+7.) Convert RGB (32,128,64) to Hexadecimal. (1pt)
 
-8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
+8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII. (1pt)
+
+9.) Reached 2000 on the Binary Game? Upload screen shot!  (7pt)
 
 '''
