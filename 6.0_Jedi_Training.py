1ec53dd729462ef1ecc2f0e6e80c454c555438a9
ngotom <ngotom@urbandaleschools.com>
2024-01-17 09:05:59 -0600

    first commit

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -1,22 +1,43 @@
 '''
-6.0 Jedi Training (15pts)  Name:________________
+6.0 Jedi Training (15pts)  Name: Tommy Ngo
 
 
 1.) Convert Binary 101010 to Hexadecimal. (1pt)
+10 | 1010
+2      A
+
+Answer: 2A
 
 2.) Convert Binary 11101001 to Octal. (1pt)
+011 | 101 | 001
+3      5     1
+
+Answer: 351
 
 3.) Convert Hexadecimal FC to Binary. (1pt)
+FC
+1111 | 1100
+
+Answer: 11111100
 
 4.) Convert Hexadecimal 1F to Decimal. (1pt)
+0001 | 1111
+
+Answer: 11111
 
 5.) Convert Hexadecimal #FAAFBD to RGB. (1pt)
+Answer: RGB(250, 175, 189)
 
 6.) Convert Octal 70 to Binary. (1pt)
+111 | 000
+
+Answer: 111000
 
 7.) Convert RGB (32,128,64) to Hexadecimal. (1pt)
+Answer: #208040
 
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII. (1pt)
+ASCII
 
 9.) Reached 2000 on the Binary Game? Upload screen shot!  (7pt)
 https://studio.code.org/projects/applab/iukLbcDnzqgoxuu810unLw
